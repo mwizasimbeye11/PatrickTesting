28f4cc71d85fcb9d987f9f94faca1c7212e9e642
Simon Cross <hodgestar@gmail.com>
2016-10-13 09:07:34 +0200

    Clean-up serializers.

--- Lima/serializers.py
+++ Lima/serializers.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
+
+""" Serializers for agents, crops and markets. """
+
 from rest_framework import serializers
-from .models import  Agent
-from .models import Crop
-from .models import Market
+
+from .models import Agent, Crop, Market
+
 
 class AgentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Agent
         fields = '__all__'
 
+
 class CropSerializer(serializers.ModelSerializer):
     class Meta:
         model = Crop
         fields = '__all__'
-class MarketsSerializer(serializers.ModelSerializer):
+
+
+class MarketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Market
         fields = '__all__'
-
