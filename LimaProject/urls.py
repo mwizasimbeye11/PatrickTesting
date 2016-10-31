1242139bb90f8610458336a0ca5889cd4d2c3319
Patrick Sikalinda <sikalindapatrick@gmail.com>
2016-10-31 14:59:55 +0200

    added rest framework token

--- LimaProject/urls.py
+++ LimaProject/urls.py
@@ -4,9 +4,11 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.authtoken import views as v
 from Lima import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^v1/', include(views.api_v1.urls, namespace='api_v1')),
+    url(r'^token/', v.obtain_auth_token),
 ]
