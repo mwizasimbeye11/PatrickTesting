1892a7ada404210fe96f6d5eb3fc49aa3e567d5f
cynthia <cynthiammulenga@gmail.com>
2016-11-01 13:58:04 +0200

    removed

--- Lima/views.py
+++ Lima/views.py
@@ -13,21 +13,21 @@ class AgentViewSet(ModelViewSet):
     """ API endpoint for creating, updating and retrieving agents. """
     queryset = Agent.objects.select_related()  # Needed for the REST Permissions
     serializer_class = AgentSerializer
-    permission_classes = (DjangoModelPermissions,)
+    #permission_classes = (DjangoModelPermissions,)
 
 
 class CropViewSet(ModelViewSet):
     """ API endpoint for creating, updating and retrieving crops. """
     queryset = Crop.objects.all()  # Needed for the REST Permissions
     serializer_class = CropSerializer
-    permission_classes = (DjangoModelPermissions,)
+    #permission_classes = (DjangoModelPermissions,)
 
 
 class MarketViewSet(ModelViewSet):
     """ API endpoint for creating, updating and retrieving crops. """
     queryset = Market.objects.all()  # Needed for the REST Permissions
     serializer_class = MarketSerializer
-    permission_classes = (DjangoModelPermissions,)
+    #permission_classes = (DjangoModelPermissions,)
 
 api_v1 = DefaultRouter()
 api_v1.register(r'agents', AgentViewSet)
