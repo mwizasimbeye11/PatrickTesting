53b367c37334c5f83d117d3259047c0889ec7d63
Mwiza Benjamin Simbeye II <mwizasimbeye@gmail.com>
2016-11-02 08:29:34 +0200

    Update views.py

--- Lima/views.py
+++ Lima/views.py
@@ -11,9 +11,19 @@ from .models import Agent, Crop, Market
 
 class AgentViewSet(ModelViewSet):
     """ API endpoint for creating, updating and retrieving agents. """
-    queryset = Agent.objects.select_related()  # Needed for the REST Permissions
+
     serializer_class = AgentSerializer
     permission_classes = (DjangoModelPermissions,)
+
+    def get_queryset(self):
+        queryset = Agent.objects.select_related()  # Needed for the REST Permissions
+        agent_id = self.request.query_params.get('agent_id', None)
+        password = self.request.query_params.get('password', None)
+        if agent_id != None:
+            if password != None:
+                password = str(password)
+                queryset = queryset.filter(agent_id=agent_id, password=password)
+        return queryset
 
 
 class CropViewSet(ModelViewSet):
@@ -30,6 +40,6 @@ class MarketViewSet(ModelViewSet):
     permission_classes = (DjangoModelPermissions,)
 
 api_v1 = DefaultRouter()
-api_v1.register(r'agents', AgentViewSet)
+api_v1.register(r'agents', AgentViewSet, 'agent')
 api_v1.register(r'crops', CropViewSet)
 api_v1.register(r'markets', MarketViewSet)
